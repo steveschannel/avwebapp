3007238e0c08d80c6ef6ee2a3239b92e18542518
steveschannel <31490008+steveschannel@users.noreply.github.com>
2020-06-03 09:50:58 -0400

    s

---
+++ dist/precache-manifest.b2d81bc5b2e8b702cac5b8d444a993d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597bdbc50b2a64249e6c",
+    "url": "/avwebapp/css/app.5329de3c.css"
+  },
+  {
+    "revision": "e6e93fc65235aa0a48ab4b8eb75bbd56",
+    "url": "/avwebapp/index.html"
+  },
+  {
+    "revision": "597bdbc50b2a64249e6c",
+    "url": "/avwebapp/js/app.e41be73a.js"
+  },
+  {
+    "revision": "137cc3ce0340cee40159",
+    "url": "/avwebapp/js/chunk-vendors.219c5ac8.js"
+  },
+  {
+    "revision": "dfb6e381f31367db35d7c2aeb8dd6ae1",
+    "url": "/avwebapp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/avwebapp/robots.txt"
+  }
+]);
